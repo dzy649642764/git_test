522348b1de16cd89d17465aa4d2b1531ba992ae6
Administrator <649642764@qq.com>
2018-12-04 16:19:53 +0800

    Init

--- src/git_test/T.java
+++ src/git_test/T.java
@@ -4,7 +4,7 @@ public class T {
 
 	public static void main(String[] args) {
 		System.out.println("nmsl");
-                System.out.println("王宏伟nmsl");
+        System.out.println("王宏伟nmsl");
 	}
 
 }
